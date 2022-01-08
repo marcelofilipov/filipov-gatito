0e78f85a04e2a9e7af7eb5cdc34244760238ee50
Marcelo Filipov <marcelo.filipov@gmail.com>
2022-01-08 18:45:36 -0300

    Stylization border implemented

--- src/components/DefaultScreen/index.js
+++ src/components/DefaultScreen/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 import { KeyboardAvoidingView, Platform, SafeAreaView, StatusBar } from 'react-native'
-import stylesGlobal from '../../styles'
 
-//import styles from './styles'
+import stylesGlobal, { colors } from '../../styles'
+import styles from './styles';
 
 export default function DefaultScreen({ children }) {
-  return <SafeAreaView style={stylesGlobal.fill}>
-    <StatusBar />
-    <KeyboardAvoidingView 
-      behavior={Platform.OS == "ios" ? "padding" : "height"}
-      style={stylesGlobal.fill}
-      >
-        {children}
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+  return <>
+    <SafeAreaView style={styles.renderScreenTOP}>
+        <StatusBar backgroundColor={colors.roxo}/>
+        <KeyboardAvoidingView 
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+        style={stylesGlobal.fill}
+        >
+            {children}
+        </KeyboardAvoidingView>
+    </SafeAreaView> 
+    <SafeAreaView style={styles.renderScreenFOOTER}/>
+  </>
 }
---
+++ src/components/DefaultScreen/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '../../styles';
+
+export default StyleSheet.create({
+  renderScreenTOP: {
+    flex: 1,
+    backgroundColor: colors.roxo,
+  },
+  renderScreenFOOTER: {
+    flex: 0,
+    backgroundColor: colors.laranja,
+  }
+});
