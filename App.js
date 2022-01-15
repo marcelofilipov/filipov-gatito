9ffab8014ae02d425b76e87b1572e9a02ee6d051
Marcelo Filipov <marcelo.filipov@gmail.com>
2022-01-15 12:11:01 -0300

    Adjustments made and Reactotron implemented

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import 'intl'
-import 'intl/locale-data/jsonp/pt-BR'
+import React from 'react';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 
-import Routes from './src/Routes'
-import DefaultScreen from './src/components/DefaultScreen'
+import Reactotron from 'reactotron-react-native';
 
-export default function App() {
-  return <DefaultScreen><Routes /></DefaultScreen>
+import Routes from './src/Routes';
+import DefaultScreen from './src/components/DefaultScreen';
+
+Reactotron.configure().useReactNative().connect();
+console.tron = Reactotron;
+
+const App = () => {
+  console.tron.log('Isto é apenas um testes...');
+  return (
+    <DefaultScreen>
+      <Routes />
+    </DefaultScreen>
+  );
 }
+
+export default App;
